3c73e79279e45e94a5c103bce55f66aaafa6bc6c
Ferenczi Zsanett <ferenczizsani@gmail.com>
2026-02-26 09:25:08 +0100

    Remove hasBg param

--- flightagram/app/[lang]/(waitlist)/waitlist/layout.tsx
+++ flightagram/app/[lang]/(waitlist)/waitlist/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function WaitlistLayout({children}: Props) {
   return (
     <>
-      <Navbar hasBg={true} />
+      <Navbar />
       {children}
       <Footer />
     </>
